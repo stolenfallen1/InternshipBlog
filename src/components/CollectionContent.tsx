13e1af562450d8ffe9608abfc3927fd58f6b3732
Paw <stolenfallen1@gmail.com>
2023-04-14 16:57:14 +0800

    Collection section progress

--- src/components/CollectionContent.tsx
+++ src/components/CollectionContent.tsx
@@ -1,31 +1,67 @@
-import React, { useState } from 'react'
-import '../styles/Collection.css'
-import img1 from '../assets/images/img-1.jpeg'
-import img2 from '../assets/images/img-2.jpeg'
-import img3 from '../assets/images/img-3.jpeg'
-import img4 from '../assets/images/img-4.jpg'
-import img5 from '../assets/images/img-5.jpg'
-import img6 from '../assets/images/img-6.jpeg'
-import img7 from '../assets/images/img-7.jpeg'
-import img8 from '../assets/images/img-8.jpg'
-import img9 from '../assets/images/img-9.jpeg'
-import img10 from '../assets/images/img-10.jpg'
+import React from 'react';
+import '../styles/Collection.css';
+import img1 from '../assets/images/img-1.jpeg';
+import img2 from '../assets/images/img-2.jpeg';
+import img3 from '../assets/images/img-3.jpeg';
+import img4 from '../assets/images/img-4.jpg';
+import img5 from '../assets/images/img-5.jpg';
+import img6 from '../assets/images/img-6.jpeg';
+import img7 from '../assets/images/img-7.jpeg';
+import img8 from '../assets/images/img-8.jpg';
+import img9 from '../assets/images/img-9.jpeg';
+import img10 from '../assets/images/img-10.jpg';
 
 const CollectionContent = () => {
-    return (
-        <div className="collection-content">
-            <img src={img1} className="images"/>
-            <img src={img2} className="images"/>
-            <img src={img3} className="images"/>
-            <img src={img4} className="images"/>
-            <img src={img5} className="images"/>
-            <img src={img6} className="images"/>
-            <img src={img7} className="images"/>
-            <img src={img8} className="images"/>
-            <img src={img9} className="images"/>
-            <img src={img10} className="images"/>
-        </div>
-    )
-}
+    const itemData = [
+        {
+        img: img1,
+        title: 'Image 1',
+        },
+        {
+        img: img2,
+        title: 'Image 2',
+        },
+        {
+        img: img3,
+        title: 'Image 3',
+        },
+        {
+        img: img4,
+        title: 'Image 4',
+        },
+        {
+        img: img5,
+        title: 'Image 5',
+        },
+        {
+        img: img6,
+        title: 'Image 6',
+        },
+        {
+        img: img7,
+        title: 'Image 7',
+        },
+        {
+        img: img8,
+        title: 'Image 8',
+        },
+        {
+        img: img9,
+        title: 'Image 9',
+        },
+        {
+        img: img10,
+        title: 'Image 10',
+        },
+    ];
 
-export default CollectionContent
+    return (
+        <div className='collection-content'>
+        {itemData.map((item) => (
+            <img key={item.title} src={item.img} className='images' alt={item.title} />
+        ))}
+        </div>
+    );
+};
+
+export default CollectionContent;
