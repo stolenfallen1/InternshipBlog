a80e4c95d55cc0ce20c1265f834b084ba16cf781
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-04-14 17:58:37 +0800

    splashscreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './styles/Main.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Hero from './Views/Hero'
-import Home from './Views/Home'
-import Collections from './Views/Collections'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./styles/Main.css";
+import Splashscreen from "./Views/SplashScreen";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Hero from "./Views/Hero";
+import Home from "./Views/Home";
+import Collections from "./Views/Collections";
 
 function App() {
+  const [showSplashscreen, setShowSplashscreen] = useState(true);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setShowSplashscreen(false);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
 
   return (
     <div className="App">
       <BrowserRouter>
+        {showSplashscreen && (
+          <div>
+            <Splashscreen />
+          </div>
+        )}
         <Routes>
-          <Route path='/' element={ <Hero />}/>
-          <Route path='/Home' element={ <Home/> }/>
-          <Route path='/Collections' element={ <Collections/> }/>
+          <Route path="/" element={<Hero />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Collections" element={<Collections />} />
         </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Views/SplashScreen.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/Main.css";
+
+const Splashscreen = () => {
+  return (
+    <div className="splash-wrap">
+      <div className="code-loader">
+        <span>{"<"}</span>
+        <span>{"/"}</span>
+        <span>{">"}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Splashscreen;
