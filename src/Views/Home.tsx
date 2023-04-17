d81a1d50a2a919ddc8d3010fa389413ded1cd7aa
Paw <stolenfallen1@gmail.com>
2023-04-17 10:52:31 +0800

    home transition effects

--- src/Views/Home.tsx
+++ src/Views/Home.tsx
@@ -11,16 +11,16 @@ const Home = () => {
     };
 
     return (
-        <>
-        <Navbar isDarkMode={isDarkMode} toggleTheme={toggleTheme}/>
-        <div className={`main-content ${isDarkMode ? "dark-mode" : ""}`}>
-            <h1 className="main-header">The key to pursuing excellence is to embrace an organic, long term 
-                learning process, and not to live in a shell of static, safe 
-                mediocrity. Usually, growth comes at the expense of previous comfort 
-                or safety.</h1>
-            <Link to="/Collections" className="home-button">Collections</Link>
+        <div className="view-home">
+            <Navbar isDarkMode={isDarkMode} toggleTheme={toggleTheme}/>
+            <div className={`main-content ${isDarkMode ? "dark-mode" : ""}`}>
+                <h1 className="main-header">The key to pursuing excellence is to embrace an organic, long term 
+                    learning process, and not to live in a shell of static, safe 
+                    mediocrity. Usually, growth comes at the expense of previous comfort 
+                    or safety.</h1>
+                <Link to="/Collections" className="home-button">Collections</Link>
+            </div>
         </div>
-        </>
     )
 }
 
