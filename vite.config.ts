ce77778a9d3df4e3969ddc8a066dd6715a94ee83
Paw <stolenfallen1@gmail.com>
2023-04-20 13:23:56 +0800

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: 'index.html',
+      output: {
+        dir: 'dist',
+        entryFileNames: '[name].[hash].js',
+        assetFileNames: '[name].[hash][extname]',
+        chunkFileNames: '[name].[hash].js',
+        format: 'es',
+        exports: 'named',
+      },
+      plugins: [],
+      external: [],
+    },
+  },
+});
