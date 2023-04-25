df2189e715197ff8db9e3026ca29e616769f3157
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-04-25 08:07:54 +0800

    overlay text in collection images

--- src/components/CollectionContent.tsx
+++ src/components/CollectionContent.tsx
@@ -5,24 +5,31 @@ import axios from "axios";
 interface Type {
   id: string;
   image: string;
+  title: string;
 }
 
 const CollectionContent = () => {
-  const [data, setData] = useState<Type[]>([])
+  const [data, setData] = useState<Type[]>([]);
 
   useEffect(() => {
     const fetchEvents = async () => {
-      const result = await axios.get("https://internship-blog-api.vercel.app/get")
-      setData(result.data)
-    }
-    fetchEvents()
-  }, [])
+      const result = await axios.get(
+        "https://internship-blog-api.vercel.app/get"
+      );
+      setData(result.data);
+    };
+    fetchEvents();
+  }, []);
 
   return (
     <div className="collection">
       {data.map((datas) => (
         <div className="image-container" key={datas.id}>
           <img src={datas.image} className="images" />
+          <div className="overlay">
+            <p className="overlay-title">{datas.title}</p>
+            <p className="overlay-text">Learn More</p>
+          </div>
         </div>
       ))}
     </div>
