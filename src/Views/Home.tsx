7fa6bca9a8889494e01475b9ca8781c9937bbce0
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-05-01 20:02:46 +0800

    collection header css to tailwind

--- src/Views/Home.tsx
+++ src/Views/Home.tsx
@@ -1,28 +1,31 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import '../styles/Home.css'
+import "../styles/Home.css";
 
 const Home = () => {
-    const [isDarkMode, setIsDarkMdoe] = useState(false)
+  const [isDarkMode, setIsDarkMdoe] = useState(false);
 
-    const toggleTheme = () => {
-        setIsDarkMdoe(prevState => !prevState);
-    };
+  const toggleTheme = () => {
+    setIsDarkMdoe((prevState) => !prevState);
+  };
 
-    return (
-        <div className="view-home">
-            <Navbar isDarkMode={isDarkMode} toggleTheme={toggleTheme}/>
-            <div className={`main-content ${isDarkMode ? "dark-mode" : ""}`}>
-                <h1 className="main-header">The key to pursuing excellence is to embrace an organic, long term 
-                    learning process, and not to live in a shell of static, safe 
-                    mediocrity. Usually, growth comes at the expense of previous comfort 
-                    or safety.</h1>
-                <Link to="/Collections" className="home-button">Collections</Link>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="view-home">
+      <Navbar isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
+      <div className={`main-content ${isDarkMode ? "dark-mode" : ""}`}>
+        <h1 className="main-header">
+          The key to pursuing excellence is to embrace an organic, long term
+          learning process, and not to live in a shell of static, safe
+          mediocrity. Usually, growth comes at the expense of previous comfort
+          or safety.
+        </h1>
+        <Link to="/Collections" className="home-button">
+          Collections
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Home
-
+export default Home;
--- src/components/CollectionHeader.tsx
+++ src/components/CollectionHeader.tsx
@@ -1,16 +1,24 @@
-import '../styles/Collection.css'
-import { Link } from 'react-router-dom'
-import { FiArrowRightCircle } from 'react-icons/fi'
+import "../styles/Collection.css";
+import { Link } from "react-router-dom";
+import { FiArrowRightCircle } from "react-icons/fi";
 
 const CollectionHeader = () => {
-    return (
-        <div className="collection-container">
-            <div className="collection-header">
-                <h1 className="collection-title">COLLECTIONS</h1>
-                <Link to="/" className="header-btn"><FiArrowRightCircle/> Home</Link>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <div className="collection-header flex flex-col justify-center items-center">
+        <h1 className="text-5xl font-bold text-white pb-5 pt-10 tracking-tighter custom-600:text-4xl custom-500:text-3xl">
+          COLLECTIONS
+        </h1>
+        <Link
+          to="/"
+          className="pb-12 text-white text-2xl transition duration-350 ease-in-out hover:opacity-70 flex items-center custom-600:text-lg custom-500:text-lg"
+        >
+          <span>Home</span>
+          <FiArrowRightCircle className="ml-2" />
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default CollectionHeader
+export default CollectionHeader;
