eae1aba78bf8aed687b05c0379efcca96bd38e02
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-05-01 18:57:51 +0800

    testing tailwind responsiveness

--- src/components/HeroContent.tsx
+++ src/components/HeroContent.tsx
@@ -4,11 +4,10 @@ import { TypeAnimation } from "react-type-animation";
 const HeroContent = () => {
   return (
     <div className="h-screen flex flex-col justify-center items-center font-montserrat">
-      <h1 className="sm:text-3xl md:text-4xl lg:text-6xl text-white mb-6 font-bold tracking-tight">
+      <h1 className="text-white mb-6 font-bold tracking-tighter xl:text-6xl lg:text-5xl md:text-5xl sm:text-4xl custom-600:text-3xl custom-500:text-3xl">
         INTERNSHIP JOURNEY
       </h1>
-
-      <p className="sm:text-2xl md:text-3xl lg:text-4xl italic text-white mb-6">
+      <p className="text-white italic mb-6 xl:text-5xl lg:text-4xl md:text-4xl sm:text-3xl custom-600:text-2xl custom-500:text-2xl">
         <TypeAnimation
           sequence={["Eat. Sleep. Code", 2500, "Live. Laugh. Love", 2500]}
           repeat={Infinity}
@@ -16,7 +15,9 @@ const HeroContent = () => {
       </p>
       <Link
         to="/Home"
-        className="text-2xl text-white bg-transparent border border-white rounded-md px-5 py-3 transition duration-500 ease-in-out hover:bg-white hover:text-black hover:border-black"
+        className="text-white bg-transparent border border-white rounded-md px-5 py-3 
+  transition duration-500 ease-in-out hover:bg-white hover:text-black hover:border-black
+  xl:text-2xl lg:py-3 lg:px-4 custom-600:px-3 custom-500:py-2 custom-500:px-2"
       >
         Learn More
       </Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ export default {
       fontFamily: {
         montserrat: ["Montserrat"],
       },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1248px",
+        "2xl": "1536px",
+        "custom-600": { min: "500px", max: "639px" },
+        "custom-500": { max: "499px" },
+      },
     },
   },
   plugins: [],
