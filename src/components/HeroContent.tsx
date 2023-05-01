88742db85254d9d335ca5e481c3d972f91d9cab1
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-05-01 14:56:21 +0800

    typing animation

--- src/components/HeroContent.tsx
+++ src/components/HeroContent.tsx
@@ -1,14 +1,22 @@
-import { Link } from "react-router-dom"
-import '../styles/Hero.css'
+import { Link } from "react-router-dom";
+import "../styles/Hero.css";
+import { TypeAnimation } from "react-type-animation";
 
 const HeroContent = () => {
-    return (
-        <div className="hero-container">
-            <h1 className="hero-title">INTERNSHIP JOURNEY</h1>
-            <p className="hero-text">Eat. Sleep. Code.</p>
-            <Link to="/Home" className="hero-button">Learn More</Link>
-        </div>
-    )
-}
+  return (
+    <div className="hero-container">
+      <h1 className="hero-title">INTERNSHIP JOURNEY</h1>
+      <p className="hero-text">
+        <TypeAnimation
+          sequence={["Eat. Sleep. Code", 2500, "Live. Laugh. Love", 2500]}
+          repeat={Infinity}
+        />
+      </p>
+      <Link to="/Home" className="hero-button">
+        Learn More
+      </Link>
+    </div>
+  );
+};
 
-export default HeroContent
+export default HeroContent;
